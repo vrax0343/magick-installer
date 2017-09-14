c84346445ceb2ca817335bff0e3a0aca932b729c
vrax0343 <vrax0343@watchtek.co.kr>
2017-09-15 01:27:20 +0900

    Update magick-installer.sh

--- magick-installer.sh
+++ magick-installer.sh
@@ -23,7 +23,7 @@ download http://voxel.dl.sourceforge.net/project/wvware/libwmf/0.2.8.4/libwmf-0.
 download http://downloads.sourceforge.net/project/lcms/lcms/1.19/lcms-1.19.tar.gz
 download http://sourceforge.net/projects/ghostscript/files/GPL%20Ghostscript/9.04/ghostscript-9.04.tar.gz
 download http://voxel.dl.sourceforge.net/project/gs-fonts/gs-fonts/8.11%20%28base%2035%2C%20GPL%29/ghostscript-fonts-std-8.11.tar.gz
-download ftp://ftp.sunet.se/pub/multimedia/graphics/ImageMagick/ImageMagick-6.6.7-0.tar.gz
+download https://launchpad.net/imagemagick/main/6.6.7-0/+download/ImageMagick-6.6.7-0.tar.gz
 
 
 tar xzvf libiconv-1.13.1.tar.gz
